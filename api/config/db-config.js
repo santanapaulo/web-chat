5fd40dbb113bf8b40d9fdd5f90dc62f0368c7084
paulo <santana.pauloh@gmail.com>
2016-03-01 19:47:07 -0300

    git status

---
+++ api/config/db-config.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var mongoose = require('mongoose')
+  , env = require('./env-config')
+  , debug = require('debug')(env.appName + ':db');
+
+// configure a mongoose connection
+mongoose.connect(env.db.uri, env.db.credentials);
+
+var db = mongoose.connection;
+
+// configure connection listeners
+db.on('connected', function () {
+  debug('MongoDB connected.');
+}).on('open', function () {
+  debug('MongoDB open.');
+}).on('disconnected', function () {
+  debug('MongoDB disconnected.');
+}).on('error', function (err) {
+  debug('MongoDB error: ' + err);
+});
+
+// configure process listener
+process.on('SIGINT', function () {
+  mongoose.connection.close(function () {
+    debug('MongoDB disconnected through app termination');
+    process.exit(0);
+  });
+});
+
+module.exports = db;
